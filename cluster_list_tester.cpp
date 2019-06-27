ead88ad0904158a6119847f089cfc842a2e893ae
Philip deZonia <pdezonia@gmail.com>
2019-06-27 12:55:44 -0400

    Made cluster list tester and a test list of consonants to test vectors of strings.

--- cluster_list_tester.cpp
+++ cluster_list_tester.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
+/*cluster_list_tester.cpp
+ * Script for testing/practicing vectors, lists, structs, and multi-file
+ * programs.*/
+
+// Forward declare cluster list function
+std::vector<std::string> con_cluster_list_test();
 
 int main() {
+	std::cout << "Testing cluster loader.\n";
+
+	std::vector<std::string> con_list = con_cluster_list_test();
+
+	std::cout << "First consonant entry: " << con_list[0] << "\n";
+
+
+	std::cout << "Done\n";
 	return 0;
 }
---
+++ con_cluster_list_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+/* con_cluster_list_test.cpp Loads a very short list of consonant clusters
+ * to help with code testing. */
+
+using namespace std;
+
+vector<string> con_cluster_list_test()
+{
+	vector<string> con_list = {"s", "d", "f", "sh", "k", "ch"};
+	return con_list;
+}
